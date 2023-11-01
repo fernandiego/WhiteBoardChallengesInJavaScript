5940b69c8bbf284724a8e0d14b561fbaeffc1227
fox <fernandodiego.sn@gmail.com>
2023-11-01 15:22:49 -0300

    Reverse a LinkedList

---
+++ reverseLinkedList.js
@@ -0,0 +1,66 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    // Add a new node to the end of the linked list
+    append(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Reverse the linked list
+    reverse() {
+        let prev = null;
+        let current = this.head;
+        let next = null;
+
+        while (current !== null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        this.head = prev;
+    }
+
+    // Print the linked list
+    print() {
+        console.log("Head:", this.head ? this.head.data : "null");
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+// Example usage:
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+console.log("Original Linked List:");
+linkedList.print();
+
+linkedList.reverse();
+
+console.log("Reversed Linked List:");
+linkedList.print();
